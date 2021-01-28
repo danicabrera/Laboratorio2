b750451cc149903328c609707187ab2b49677ab3
Daniel Cabrera <dandrescabrerap@gmail.com>
2021-01-28 12:26:18 -0600

    Commit clase de la mañana 28-01

--- app/src/main/java/com/example/proyecto1/MainActivity.kt
+++ app/src/main/java/com/example/proyecto1/MainActivity.kt
@@ -7,9 +7,14 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        variablesYConstantes()
-        sentenciaIf()
-        sentenciaWhen()
+        //variablesYConstantes()
+        //sentenciaIf()
+        //sentenciaWhen()
+        arrays()
+        maps()
+        loops()
+
+
     }
 
     // Variables y Constantes
@@ -81,4 +86,87 @@ class MainActivity : AppCompatActivity() {
                 println("No conocemos el idioma")
         }
         }
+    }
+
+    private fun arrays() {
+        val nombre = "Daniel"
+        val apellido = "Cabrera"
+        val ciudad = "Guatemala"
+        val universidad = "Del Valle de Guatemala"
+
+
+        val myArray:ArrayList<String> = arrayListOf<String>()
+        //Agregar Datos
+        myArray.add(nombre)
+        myArray.add(apellido)
+        myArray.add(ciudad)
+        myArray.add(universidad)
+
+        println(myArray)
+
+        //Agregar conjunto de datos
+
+        myArray.addAll(listOf("Campus Uvg", "El pepe"))
+        println(myArray)
+
+        //Acceso a Datos
+        val myCompany:String = myArray[2]
+        println(myCompany)
+
+        //Modificar datos
+        myArray[2] = "Solo del Valle"
+
+
+        //eliminar datos
+        myArray.removeAt(2)
+        println(myArray)
+
+        //Recorrer el Array
+        myArray.forEach(){
+            //println()
+        }
+
+    }
+
+    fun maps(){
+        var myMap : Map<String, Int> = mapOf()
+        println(myMap)
+
+        //agregar datos
+
+        myMap   = mutableMapOf("uno" to 1, "dos" to 2, "tres" to 3, "cuatro" to 4)
+        println(myMap)
+
+        //agregar dato 1
+        myMap["cinco"] = 5
+        myMap["seis"] = 6
+
+        println(myMap)
+
+        myMap.put("siete", 7)
+        println(myMap)
+
+        //Acceso a datos
+        println(myMap["seis"])
+
+        //eliminar los datos
+        myMap.remove("seis")
+        println(myMap)
+
+    }
+
+    fun loops(){
+        //For
+        val myArray: List<String> = listOf("Daniel", "Cabrera", "Guatemala", "Del valle de guatemala")
+        val myMap: MutableMap<String, Int> = mutableMapOf("uno" to 1, "dos" to 2, "tres" to 3, "cuatro" to 4)
+        for(myString: String in myArray){
+            println(myString)
+        }
+
+        for(myElement:MutableMap.MutableEntry<String, Int> in myMap){
+            println("${myElement.key} - ${myElement.value}")
+        }
+
+        
+        //While
     }
